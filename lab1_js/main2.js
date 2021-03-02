37cbf733bbe95033b36e70951913d54b2174ee41
xiahanchen <xiahanchen@xiahanchendeMacBook-Pro.local>
2021-03-02 14:08:26 +0800

    lab 1 exercise

---
+++ lab1_js/main2.js
@@ -0,0 +1,5 @@
+var a = 3;
+var b = 5;
+var c = 7;
+var str = "sum of numbers is:";
+console.log(str + String(a + b + c));
---
+++ lab1_js/main2.ts
@@ -0,0 +1,5 @@
+var a=3
+var b=5
+var c=7
+var str:String ="sum of numbers is:"
+console.log(str+String(a+b+c))
